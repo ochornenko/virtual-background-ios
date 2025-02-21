e0f19ef5b4ff277709cb0c187d06599c55f4bce3
Oleg Chornenko <oleg.chornenko@gmail.com>
2025-02-20 23:02:08 -0500

    Create initial project

---
+++ App/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  VirtualBackground
+//
+//  Created by Oleg Chornenko on 2/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
